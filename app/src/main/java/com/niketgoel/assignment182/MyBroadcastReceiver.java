66e48ed14751fa1ff27908cab83268df5982c1d8
Niket Goel <niket03@gmail.com>
2018-01-03 21:22:54 +0530

    Initial commit

---
+++ app/src/main/java/com/niketgoel/assignment182/MyBroadcastReceiver.java
@@ -0,0 +1,23 @@
+package com.niketgoel.assignment182;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by niketgoel on 03/01/18.
+ */
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    private String TAG = "MyBroadcastReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received system broadcast");
+        Intent intentStartMainActivity = new Intent(context,
+                MainActivity.class);
+        intentStartMainActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intentStartMainActivity);
+    }
+}
